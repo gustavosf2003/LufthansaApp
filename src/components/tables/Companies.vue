b7b052492432b487cb088bad01faa2522e088024
Gustavo Ferreira <gustavo.ferreira@orbital.company>
2021-09-24 16:53:09 -0300

    Table company dinamic

--- src/components/tables/Companies.vue
+++ src/components/tables/Companies.vue
@@ -1,43 +1,41 @@
 <template>
     <section class="mt-3">
         <b-table
-            :data="isEmpty ? [] : data"
+            :data="isEmpty ? {} : flights"
             :striped="true"
             :hoverable="true"
-            :mobile-cards="hasMobileCards">
-
-            <b-table-column field="id" label="ID" width="40" :td-attrs="columnTdAttrs" numeric v-slot="props">
+            :mobile-cards="true">
+            
+            <b-table-column field="id" label="ID" width="40"  numeric v-slot="props">
                 {{ props.row.id }}
             </b-table-column>
-
-            <b-table-column field="destiny" label="Destino" :td-attrs="columnTdAttrs" v-slot="props">
+            <b-table-column field="destiny" label="Destino"  v-slot="props">
                 {{ props.row.destiny }}
             </b-table-column>
-
-            <b-table-column field="company" label="Empresa" :td-attrs="columnTdAttrs" v-slot="props">
+            <b-table-column field="company" label="Empresa"  v-slot="props">
                 {{ props.row.company }}
             </b-table-column>
-            <b-table-column field="company" label="País da Empresa" :td-attrs="columnTdAttrs" v-slot="props">
+            <b-table-column field="companyCountry" label="País da Empresa"  v-slot="props">
                 {{ props.row.companyCountry }}
             </b-table-column>
-            <b-table-column field="date" label="Duração" :th-attrs="dateThAttrs" :td-attrs="columnTdAttrs" centered v-slot="props">
+            <b-table-column field="duration" label="Duração" centered v-slot="props">
                 <span class="tag is-success">
                     {{ props.row.duration }}
                 </span>
             </b-table-column>
-            <b-table-column label="Escala" :td-attrs="columnTdAttrs" v-slot="props">
+            <b-table-column field="scale" label="Escala"  v-slot="props">
                 <span>
                     <b-icon
                         v-if="props.row.id !== 'Total'"
                         pack="fas"
-                        :icon="props.row.escala === 'sim' ? 'check' : 'venus'">
+                        :icon="props.row.scale === 'sim' ? 'check' : 'venus'">
                     </b-icon>
                 </span>
             </b-table-column>
-            <b-table-column field="company" label="Taxa" :td-attrs="columnTdAttrs" v-slot="props">
+            <b-table-column field="tax" label="Taxa"  v-slot="props">
                 {{ props.row.tax }}
             </b-table-column>
-            <b-table-column field="date" label="Valor" :th-attrs="dateThAttrs" :td-attrs="columnTdAttrs" centered v-slot="props">
+            <b-table-column field="price" label="Valor" centered v-slot="props">
                 <span class="tag is-success">
                     {{ props.row.currency + props.row.price }}
                 </span>
@@ -45,21 +43,31 @@
             <template #empty>
                 <div class="has-text-centered">Parece que você ainda não possui voos criados</div>
             </template>
-
         </b-table>
+        <h1>{{flights}}</h1>
     </section>
 </template>
 
 <script>
     export default {
         data() {
-            const data = [
-                { 'id': 1, 'destiny': 'Estocolmo', 'company': 'TAP', 'companyCountry': 'Portugal', 'duration': '2:00', 'escala': 'sim', 'tax': 1.0, 'currency': '$', 'price': '1000' },
-            ]
+            const data = []
             return {
                 data,
                 isEmpty: false,
+                flights: {}
             }
         },
+        methods:{
+            async getFlights(){
+                const req = await fetch("http://localhost:3000/flights")
+                const flights = await req.json()
+                this.flights = flights
+                
+            }
+        },
+        mounted(){
+            this.getFlights()
+        }
     }
 </script>
